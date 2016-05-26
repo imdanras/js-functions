d1d23018b77a982edf78c1570467a2dba8489e32
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-05-26 13:46:16 -0700

    number times its index in an array

---
+++ src/js/sillySum.js
@@ -0,0 +1,18 @@
+var sumArray = [ 1, 2, 3, 4];
+
+count = 0;
+
+
+var mult = function(arr) {
+    for (i = 0; i < sumArray.length; i++) {
+      count = (i * sumArray[i]);
+      console.log(count);
+
+ };
+
+};
+
+mult(sumArray);
+
+
+
